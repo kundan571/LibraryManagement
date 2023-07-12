95dcc53ae1fb15529ec8808cd0b49f2569c3d942
kundan kumar <kundanchauhan1997@gmail.com>
2023-07-12 09:54:50 +0530

    Added: Adding some book to library

--- Library.cs
+++ Library.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace LibraryManagementSystem;
 
 public class Library
 {
-    
+    private string connectionString = "Data Source=KUNDAN;Initial Catalog=LibraryManagementDB;Integrated Security=True;";
+
+    public string Name { get; set; }
+
+    public Library(string name)
+    {
+        this.Name = name;
+    }
+
+    public void AddBook(Book book)
+    {
+        using(SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            using(SqlCommand command = new SqlCommand("AddBook",connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@BookId", book.BookId);
+                command.Parameters.AddWithValue("@Title", book.Title);
+                command.Parameters.AddWithValue("@Author", book.Author);
+                command.Parameters.AddWithValue("@Genre", book.Genre);
+                command.Parameters.AddWithValue("@IsBorrowed", book.IsBorrowed);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,24 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome To Library System");
+        Library library = new Library("SchoolLibrary");
+        bool menu = true;
+        while (menu)
+        {
+            Console.WriteLine("Enter Menu:");
+            Console.WriteLine("Enter 1 to Add Book:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    library.AddBook(new Book(1, "The Da Vinci Code", " Dan Brow", "Mystry", true));
+                    library.AddBook(new Book(2, "Harry Potter", "J.K Rowling", "Fantsy", true));
+                    library.AddBook(new Book(3, "RashmiRathi", "Dinker", "Mythology", true));
+                    break;
+                default:
+                    menu = false;
+                    break;
+            }
+        }
     }
 }
